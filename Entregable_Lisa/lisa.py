0c681c3048e6effaa33cae0c8f643cd7874c0875
darydaro <100683177+fernandezdario@users.noreply.github.com>
2022-10-21 12:15:10 +0200

    Entregable 1

--- Entregable_Lisa/lisa.py
+++ Entregable_Lisa/lisa.py
@@ -23,16 +23,13 @@ while True :
     URL_imagen = requests.get(imagen).content #antes estaba en el if, el elif y el de lista general
     imagen_local = f"Entregable_Lisa/{personaje}.png" 
 
-    
     if personaje == 'Homer Simpson':
         lista_homer.append((personaje, frase))
         lista_general.append((personaje,frase)) 
 
-
     elif personaje == 'Lisa Simpson':
         lista_general.append((personaje, frase))
         lista_lisa.append((personaje, frase))
-
 
     lista_general.append((personaje, frase)) 
     
@@ -42,21 +39,20 @@ while True :
     
         if personaje == 'Homer Simpson':
             my_dict1 = {"personaje": personaje,"frase": frase}
-            with open('C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\{personaje}\{personaje}.csv', 'a') as g: 
+            with open(f'C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\{personaje}\{personaje}.csv', 'a') as g: 
                 a = csv.DictWriter(g, my_dict1.keys())
                 a.writerow(my_dict1)
 
         elif personaje == 'Lisa Simpson':
             my_dict2 = {"personaje": personaje, "frase": frase}
-            with open('C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\{personaje}\{personaje}.csv', 'a') as h: 
+            with open(f'C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\{personaje}\{personaje}.csv', 'a') as h: 
                 a = csv.DictWriter(h, my_dict2.keys()) 
                 a.writerow(my_dict2) 
 
-        else:
-            my_dict0 = {"personaje": personaje, "frase": frase}
-            with open('C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\general_1.csv', 'a') as f: 
-                a = csv.DictWriter(f, my_dict0.keys())
-                a.writerow(my_dict0)
+        my_dict0 = {"personaje": personaje, "frase": frase}
+        with open(f'C:\\Users\dario\OneDrive\\Documentos\\GitHub\\Entregable-1\\Entregable_Lisa\\general_1.csv', 'a') as f: 
+            a = csv.DictWriter(f, my_dict0.keys())
+            a.writerow(my_dict0)
     
         with open(imagen_local, 'wb') as handler:
             handler.write(URL_imagen) 
@@ -65,7 +61,6 @@ while True :
         if e.errno != errno.EEXIST:
             raise
     
-
     separador = frase.split()
     for palabra in separador:
         palabra = palabra.translate(str.maketrans('', '', string.punctuation))
@@ -79,7 +74,6 @@ while True :
       for clave, valor in contador_palabras.items():
         contador.write(f"\n{clave}: {valor}")
         
-    print(contador_palabras)
-    time.sleep(0)
+    time.sleep(1)
 
 
